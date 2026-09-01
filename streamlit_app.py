436aa1687c31dd6f459d0b37d99ff4980fd7009d
jaysterling11 <167261997+jaysterling11@users.noreply.github.com>
2026-09-01 16:00:26 +0000

    updated title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-lab1_page = st.Page("Lab1.py", title="Lab 1", icon="1️⃣")
+lab1_page = st.Page("Lab1.py", title="Lab 1 - Build a Streamlit Document ‘Q&A’ app", icon="1️⃣")
 lab2_page = st.Page("Lab2.py", title="Lab 2 - Document Summarizer", icon="2️⃣", default=True)
 
 pg = st.navigation([lab1_page, lab2_page])
