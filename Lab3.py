3c18834adee2e43d3c5e611b42ab77a15b5eba33
jaysterling11 <167261997+jaysterling11@users.noreply.github.com>
2026-09-08 16:00:04 +0000

    fixes

--- Lab3.py
+++ Lab3.py
@@ -4,8 +4,11 @@ import numpy as np
 
 st.title("MY Lab 3 question answering chatbot")
 
-openAI_model = st.sidebar.selectbox("Which Model?",
-                                    ("mini", "regular"))
+openAI_model = st.sidebar.selectbox(
+    "Which Model?",
+    ("mini", "regular")
+)
+
 if openAI_model == "mini":
     model_to_use = "gpt-4o-mini"
 else:
@@ -16,8 +19,12 @@ if 'client' not in st.session_state:
     st.session_state.client = OpenAI(api_key=openai_api_key)
 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = \
-        [{"role": "assistant", "content": "Hello! I am your chatbot. How can I assist you today?"}]
+    st.session_state["messages"] = [
+        {
+            "role": "assistant",
+            "content": "Hello! I am your chatbot. How can I assist you today?"
+        }
+    ]
 
 for msg in st.session_state.messages:
     chat_msg = st.chat_message(msg["role"])
@@ -27,11 +34,16 @@ if prompt := st.chat_input("Ask me anything!"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
+
     client = st.session_state.client
     stream = client.chat.completions.create(
         model=model_to_use,
         messages=st.session_state.messages,
-        stream=True)
+        stream=True
+    )
+
+    
 with st.chat_message("assistant"):
     response = st.write_stream(stream)
-st.session_state.messages.append({"role": "assistant", "content": response["content"]})
+
+st.session_state.messages.append({"role": "assistant", "content": response})
