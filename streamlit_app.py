5be7ca415e275b26638fadb83e656fb18344a8df
jaysterling11 <167261997+jaysterling11@users.noreply.github.com>
2026-09-08 10:27:15 -0400

    Add Lab 3 page to Streamlit app navigation

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 lab1_page = st.Page("Lab1.py", title="Lab 1 - Build a Streamlit Document ‘Q&A’ app", icon="1️⃣")
 lab2_page = st.Page("Lab2.py", title="Lab 2 - Document Summarizer", icon="2️⃣", default=True)
+lab3_page = st.Page("Lab3.py", title="Lab 3 - Streaming Chatbot with Memory", icon="3️⃣")
 
-pg = st.navigation([lab1_page, lab2_page])
+pg = st.navigation([lab1_page, lab2_page, lab3_page])
 pg.run()
